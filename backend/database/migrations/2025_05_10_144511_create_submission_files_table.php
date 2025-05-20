37660dd3cf6fc6cc5178b4135203b1dce61f443d
Bl@ze <123462762+BlazingTwister@users.noreply.github.com>
2025-05-20 19:06:41 +0000

    Update 2025_05_10_144511_create_submission_files_table.php

--- backend/database/migrations/2025_05_10_144511_create_submission_files_table.php
+++ backend/database/migrations/2025_05_10_144511_create_submission_files_table.php
@@ -6,16 +6,14 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('submission_files', function (Blueprint $table) {
             $table->id();
             $table->foreignId('student_submission_id')->constrained('student_submissions')->onDelete('cascade');
             $table->string('file_name'); // Original file name
-            $table->string('file_path'); // Stored path (e.g., in storage/app/submissions)
+            $table->string('file_path'); // Stored path
             $table->unsignedBigInteger('file_size');
             $table->string('mime_type')->nullable();
             $table->timestamp('uploaded_at')->useCurrent();
@@ -23,9 +21,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('submission_files');
