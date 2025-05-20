7493a75f9b44069faa347413938581e72b8cd782
Bl@ze <123462762+BlazingTwister@users.noreply.github.com>
2025-05-20 19:05:47 +0000

    Update 2025_05_10_144502_create_submission_slot_student_table.php

--- backend/database/migrations/2025_05_10_144502_create_submission_slot_student_table.php
+++ backend/database/migrations/2025_05_10_144502_create_submission_slot_student_table.php
@@ -6,24 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+ 
     public function up(): void
     {
         Schema::create('submission_slot_student', function (Blueprint $table) {
             $table->id();
             $table->foreignId('submission_slot_id')->constrained('submission_slots')->onDelete('cascade');
             $table->foreignId('student_id')->constrained('users')->onDelete('cascade'); // Links to the student
-            $table->timestamp('posted_at')->useCurrent(); // When the slot was made visible to this student
+            $table->timestamp('posted_at')->useCurrent(); 
             $table->unique(['submission_slot_id', 'student_id']); // Ensure a student is assigned to a slot only once
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+  
     public function down(): void
     {
         Schema::dropIfExists('submission_slot_student');
